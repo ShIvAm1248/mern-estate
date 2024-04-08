20a255cdc4564c0aa745c0cf0659fd8ca89fc7e8
ShIvAm1248 <shivam.thakr77@gmail.com>
2024-04-08 22:48:30 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.router.js';
 import cookieParser from "cookie-parser";
+import path from 'path';
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,6 +17,8 @@ mongoose
     console.log(err);
   });
 
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,6 +33,11 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
